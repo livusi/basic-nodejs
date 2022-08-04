bfda21946efb479baf941203993f2366b5c0d017
Gustavo Ramirez Rugerio <gustavoramirezrugerio@gmail.com>
2022-08-03 20:33:42 -0500

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Demo sin CI!!! ');
+  res.send('Hello World Demo sin CI V2!!! ');
 });
 
 app.listen(PORT, HOST);
